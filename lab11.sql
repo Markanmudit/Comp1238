707107ea2540ddfc09d4e64cadc7ec2a03c7b659
Markanmudit <113790078+Markanmudit@users.noreply.github.com>
2024-11-14 10:21:47 -0500

    Update lab11.sql

--- lab11.sql
+++ lab11.sql
@@ -23,3 +23,12 @@ WHERE status != 'Completed'
   AND course_id LIKE 'COMM%'
   AND due_date < '2024-12-31'
 ORDER BY due_date;
+
+-- Exercise Question 
+SELECT title,due_date
+FROM assignments
+WHERE course_id ='COMP1234'
+
+SELECT title,due_date
+FROM assignments
+WHERE due_date LIKE '2024-10%';
